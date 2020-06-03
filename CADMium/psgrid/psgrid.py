3a9ea2ecbe73c0838943c332aed540cbffc6bdcf
Victor H. Chavez <gonza445@purdue.edu>
2020-06-03 12:32:51 -0400

    Update documentation

--- CADMium/psgrid/psgrid.py
+++ CADMium/psgrid/psgrid.py
@@ -35,66 +35,67 @@ class Psgrid():
     def __init__(self, NP, NM, a, L, loc):
 
         #Mesh properties
-        self.NP = NP
-        self.NMa = NM[0]
-        self.NMr = NM[1]
-        self.Na = None
-        self.Nr = None
-        self.Nelem = None
+        self.NP = NP        #Number of points per integration block
+        self.NMa = NM[0]    #Number of angular blocks
+        self.NMr = NM[1]    #Number of radial blocks
+        self.Na = None      #Number of angular points
+        self.Nr = None      #Number of radial points
+        self.Nelem = None   #Total number of elements (points?)
 
         #Prolate Spheroidal Coordinates
-        self.xa = None
-        self.xr = None
-        self.Xa = None
-        self.Xr = None
-        self.ha = None
-        self.hr = None
+        self.xa = None      #Angular coordinate in 1d array
+        self.xr = None      #Radial coordinate in 1d array
+        self.Xa = None      #Angular coordinate in 2d grid
+        self.Xr = None      #Radial coordinate in 2d grid
+        self.ha = None      #Angular grid spacing
+        self.hr = None      #Radial grid spacing
 
         #Cartesian coordinates
         self.Y = None
         self.Z = None
 
         #Constants
-        self.a = a
-        self.R = None
-        self.L = L
+        self.a = a          #Half bond length
+        self.R = None       #Bond length
+        self.L = L          #Spheroidal box size
 
         #volume element and integration weights
-        self.w = None
-        self.wi = None
-        self.f = None
+        self.w = None       #Volume element
+        self.wi = None      #Integration weights
+        self.f = None       #Orbital angular momentum potential
 
         #Finite difference stencils
-        self.d1 = None
-        self.i1 = loc
-        self.d2 = None
-        self.i2 = loc
+        self.d1 = None      #First order coefficients
+        self.i1 = loc       #Location
+        self.d2 = None      #Second order coefficients
+        self.i2 = loc       #Location
 
         #Basic finite difference operators
         # m -> even symmetry
-        self.eDa1 = None
-        self.eDa2 = None
-        self.eDr1 = None
-        self.eDr2 = None
+        self.eDa1 = None    #Angular differentiator
+        self.eDa2 = None    #Angular differentiator
+        self.eDr1 = None    #Radial differentiator
+        self.eDr2 = None    #Radial differentiator
         # -> odd symmetry
-        self.oDa1 = None
-        self.oDa2 = None
-        self.oDr1 = None
-        self.oDr2 = None
+        self.oDa1 = None    #Angular differentiator
+        self.oDa2 = None    #Angular differentiator
+        self.oDr1 = None    #Radial differentiator
+        self.oDr2 = None    #Radial differentiator
 
         #Prolate spheroidal operators
-        self.elap = None
-        self.olap = None
-        self.grada = None
-        self.gradr = None
-        self.diva = None
-        self.divr = None
+        self.elap = None    #Laplacian -> even
+        self.olap = None    #Laplacian -> odd
+        self.grada = None   #Angular gradient component (we only need m=even gradient)
+        self.gradr = None   #Radial gradient component
+        self.diva = None    #Angular divergence component (we only need m=even gradient)
+        self.divr = None    #Radial divergence component
 
         #Boundary Conditions
-        self.bcN = None
-        self.bc1 = None
-        self.blap = None
-        self.bXa = None
+        self.bcN = None     #Size of boundary region
+        self.bc1 = None     #Outer radial boundary conditions 1st order
+        self.bc2 = None     #Outer radial boundary conditions 2nd order
+        self.blap = None    #Laplacian for values beyond Xr=L boundary
+        self.bXa = None     #Coordinates just outsise the Xr=Boundary
         self.bXr = None
 
         #Scale Factors
@@ -113,7 +114,6 @@ class Psgrid():
 
     def mirror(self, fin):
         return mirror(self, fin)
-
 
     def square(self, fin):
         return square(self, fin)
