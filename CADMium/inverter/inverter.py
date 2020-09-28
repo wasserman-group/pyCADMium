00a0bd244d25151b1c6836970708a0ddb03583fb
Victor H. Chavez <gonza445@purdue.edu>
2020-09-28 17:09:07 -0400

    update on WuYang method

--- CADMium/inverter/inverter.py
+++ CADMium/inverter/inverter.py
@@ -2,61 +2,62 @@
 inverter.py
 """
 
+import numpy as np
 from .linresponse import linresponse
+# from .linresponse import Ws
+# from .linresponse import Jacobian
 
 class Inverter():
+    """
+    Inverter handles inversion algorithms
+
+    Parameters 
+    ----------
+
+    Attributes
+    ----------
+    vs : numpy array
+        Kohn Sham Potential
+    us : float
+        Chemical Potential
+    ts_WFI :
+        Kinetic energy density using laplacian
+    ts_WFII :
+        Kinetic energy density using gradient
+    Ts : float
+        Kinetic Energy
+
+
+    Defaults
+    --------
+    Convergence and Algorithm Parameters
+
+    Use_Iterative : logical
+        Set to True to use iterative sovler with orbital invert or eigesolveinvert.
+        Otherwise direct solver will be used which may take a long time for large
+        grids.
+
+    Kval : floag
+        Parameter for iterative preconditioner
+
+    TolLinsolve : float
+        Tolerance for iterative solution to linear solve
+
+    MaxIterLinsolve: int
+        Maximum number of iterations for solution to linear solve
+
+    TolInvert: float
+        Requested tolerance for inversion
+
+    MaxIterInvert: int
+        Maximum iterations for inversions
+
+    ResFactor: 1e0
+        Determines choice of update in orbitalinvert
+        
+    """
 
     def __init__(self, grid, solver, optInversion):
-        """
-        Inverter handles inversion algorithms
-
-        Parameters 
-        ----------
-
-        Attributes
-        ----------
-        vs : numpy array
-            Kohn Sham Potential
-        us : float
-            Chemical Potential
-        ts_WFI :
-            Kinetic energy density using laplacian
-        ts_WFII :
-            Kinetic energy density using gradient
-        Ts : float
-            Kinetic Energy
-
-
-        Defaults
-        --------
-        Convergence and Algorithm Parameters
-
-        Use_Iterative : logical
-            Set to True to use iterative sovler with orbital invert or eigesolveinvert.
-            Otherwise direct solver will be used which may take a long time for large
-            grids.
-
-        Kval : floag
-            Parameter for iterative preconditioner
-
-        TolLinsolve : float
-            Tolerance for iterative solution to linear solve
-
-        MaxIterLinsolve: int
-            Maximum number of iterations for solution to linear solve
-
-        TolInvert: float
-            Requested tolerance for inversion
-
-        MaxIterInvert: int
-            Maximum iterations for inversions
-
-        ResFactor: 1e0
-            Determines choice of update in orbitalinvert
-            
-                    """
-
-
         optInversion["AB_SYM"] = optInversion["AB_SYM"] if "AB_SYM" in optInversion.keys() else False
         optInversion["ENS_SPIN_SYM"] = optInversion["ENS_SPIN_SYM"] if "ENS_SPIN_SYM" in optInversion.keys() else False
         optInversion["USE_ITERATIVER"] = optInversion["USE_ITERATIVE"] if "USE_ITERATIVE" in optInversion.keys() else False
@@ -87,32 +88,76 @@ class Inverter():
         Do the inverstion
         """
 
-        flag = None
-        output = None
+        flag = []
+        output = []
 
         if self.optInversion["invert_type"] == "wuyang":
-            print("I'm inverting th")
+            print("I'm inverting with wuyang")
             flag, output = self.linresponse(n0, vs0, ispin)
 
-        elif self.optInversion["invert_type"] == "simple":
-            self.simple(n0, vs0, ispin)
+        # elif self.optInversion["invert_type"] == "simple":
+        #     self.simple(n0, vs0, ispin)
 
-        elif self.optInversion["invert_type"] == "orbitalinvert":
-            flag, output = self.orbitalinvert(n0, vs0, phi0, e0, ispin)
+        # elif self.optInversion["invert_type"] == "orbitalinvert":
+        #     flag, output = self.orbitalinvert(n0, vs0, phi0, e0, ispin)
 
-        elif self.optInversion["invert_type"] == "qinvert":
-            flag, output = self.qinvert(n0, vs0, phi0, e0, ispin, Qi)
+        # elif self.optInversion["invert_type"] == "qinvert":
+        #     flag, output = self.qinvert(n0, vs0, phi0, e0, ispin, Qi)
 
-        elif self.optInversion["invert_type"] == "eigensolveinvert":
-            flag, output = self.eigensolveinvert(n0, vs0, ispin)
+        # elif self.optInversion["invert_type"] == "eigensolveinvert":
+        #     flag, output = self.eigensolveinvert(n0, vs0, ispin)
 
-        elif self.optInversion["invert_type"] == "test":
-            flag, output = self.test(n0, vs0, phi0, e0, ispin)
-        else:
-            raise ValueError(f"{self.optInversion['invert_type']} is not an available inversion method")
+        # elif self.optInversion["invert_type"] == "test":
+        #     flag, output = self.test(n0, vs0, phi0, e0, ispin)
+        # else:
+        #     raise ValueError(f"{self.optInversion['invert_type']} is not an available inversion method")
 
         return flag, output
 
     #Inversion methods
     def linresponse(self, n0, vs0, ispin):
         flag, output = linresponse(self, n0, vs0)
+
+    def Ws(self, vs):
+        """
+        Calculates G for a given potential
+        """
+
+        if self.optInversion["AB_SYM"] is True:
+            vs = 0.5 * (vs + self.grid.mirror(vs))
+
+        #Transfer new potentials to solver objects and calculate new densities
+        self.solver[0,0].setveff(vs)
+        self.solver[0,0].calc_orbitals()
+        self.solver[0,0].calc_density()
+        self.solver[0,0].calc_energy()
+        self.solver[0,0].calc_response()
+
+
+        #Calculate new density      
+        n = np.zeros((self.grid.Nelem, self.pol))  
+        for i in range(self.Nmo.shape[0]):
+            for j in range(self.Nmo.shape[1]):
+                n[:,j] += np.squeeze(self.solver[i,j].n)
+
+        if self.optInversion["AB_SYM"] is True:
+            n = 0.5 * (n + self.grid.mirror(n))
+
+        #Calculate error function
+        grad = np.hstack( ( B @ (n-n0), self.vs ) )
+
+        return grad
+
+    def Jacobian(self, vs):
+        """
+        Calculates Jacobian for a given vs
+        """
+
+        #Calculate jacobian of error function
+        Jac     = np.hstack( ( B @ self.solver[0,0].chi, np.zeros((1, Nelem)) ) )
+        Jac[-1] = 1
+
+        if optInversion["AB_SYM"] is True:
+            Jac[-1, :] = 0.5 * (Jac[-1,:] + self.grid.mirror(Jac[-1,:]))
+
+        return Jac
--- CADMium/inverter/linresponse.py
+++ CADMium/inverter/linresponse.py
@@ -6,43 +6,33 @@ import sys
 
 import numpy as np
 from scipy.optimize import least_squares
+from scipy.optimize import minimize
 from scipy.sparse import spdiags
 
-def Ws(vs, self):
-    """
-    Calculates G and its gradient for a given potential
-    """
 
-    if self.optInversion["AB_SYM"] is True:
-        vs = 0.5 * (vs + self.grid.mirror(vs))
+# def Jacobian(self, vs):
+#     """
+#     Calculates Jacobian of a given vs
+#     """
 
-    #Transfer new potentials to solver objects and calculate new densities
-    self.solver[0,0].setveff(vs)
-    self.solver[0,0].calc_orbitals()
-    self.solver[0,0].calc_density()
-    self.solver[0,0].calc_energy()
-    self.solver[0,0].calc_response()
+#     if self.optInversion["AB_SYM"] is True:
+#         vs = 0.5 * (vs + self.grid.mirror(vs))
 
-    #Calculate new density      
-    n = np.zeros((self.grid.Nelem, self.pol))  
-    for i in range(self.Nmo.shape[0]):
-        for j in range(self.Nmo.shape[1]):
-            n[:,j] += np.squeeze(self.solver[i,j].n)
+#     #Transfer new potentials to solver objects and calculate new densities
+#     self.solver[0,0].setveff(vs)
+#     self.solver[0,0].calc_orbitals()
+#     self.solver[0,0].calc_density()
+#     self.solver[0,0].calc_energy()
+#     self.solver[0,0].calc_response()
 
-    if self.optInversion["AB_SYM"] is True:
-        n = 0.5 * (n + self.grid.mirror(n))
+#     #Calculate new density      
+#     n = np.zeros((self.grid.Nelem, self.pol))  
+#     for i in range(self.Nmo.shape[0]):
+#         for j in range(self.Nmo.shape[1]):
+#             n[:,j] += np.squeeze(self.solver[i,j].n)
 
-    #Calculate error function
-    grad = np.hstack( ( B @ (n-n0), self.vs ) )
-
-    #Calculate jacobian of error function
-    Jac     = np.hstack( ( B @ self.solver[0,0].chi, np.zeros((1, Nelem)) ) )
-    Jac[-1] = 1
-
-    if optInversion["AB_SYM"] is True:
-        Jac[-1, :] = 0.5 * (Jac[-1,:] + self.grid.mirror(Jac[-1,:]))
-
-    return grad
+#     if self.optInversion["AB_SYM"] is True:
+#         n = 0.5 * (n + self.grid.mirror(n))
 
 def linresponse(self, n0, vs0=None):
     """
@@ -51,11 +41,12 @@ def linresponse(self, n0, vs0=None):
 
     n0 = n0[:, None]
     pol   = n0.shape[1] if len(n0.shape) > 1 else 1
+    self.pol = pol
     Nelem = n0.shape[0]
     w = self.grid.w if pol == 1 else np.hstack((self.grid.w, self.grid.w))
     
     if self.solver[0,0].veff is None:
-        if vs0 == None:
+        if vs0 is None:
             #If no initial guess is provided
             #Use the von weizacker inversion
             vs0 = (0.5 * self.grid.elap @ (n0**0.5)) / (n0**0.5 * w) 
@@ -67,6 +58,7 @@ def linresponse(self, n0, vs0=None):
     self.us = np.zeros((1, pol))
 
     if self.optInversion["ENS_SPIN_SYM"] is True:
+        print("Im inverting with symmetry")
         #Invert density n0 to find vs
         i = 1
         #Preallocation
@@ -75,7 +67,13 @@ def linresponse(self, n0, vs0=None):
 
         self.solver[0,0].hamiltonian()
         self.solver[0,0].e0 = -20
-        self.vs[:, i] = least_squares(Ws, vs0[:, i])
+
+        res_lsq = least_squares(fun    = self.Ws,
+                                x0     = vs0[:, i], 
+                                jac    = self.Jacobian, 
+                                method = "trf")
+        
+        #self.vs[:, i] = resultado de least squares
 
         self.us = self.solver[0,0].get_homo()
         self.vs[:,1] = self.vs[:, 0]
@@ -84,15 +82,23 @@ def linresponse(self, n0, vs0=None):
 
     else:
         for i in range(pol):
+            print("Im inverting without symmetry")
             #Invert density n0 to find vs
 
             #Preallocation
             B = spdiags(1./n0[:, i], 0, Nelem, Nelem)
-
             self.solver[0,0].hamiltonian()
             self.solver[0,0].e0 = -20
-            self.vs[:, i] = least_squares(Ws, vs0[:,i])
+
+            res_lsq = least_squares(fun    = self.Ws,
+                                    x0     = vs0[:, i], 
+                                    jac    = self.Jacobian, 
+                                    method = "trf")
+
+            # self.vs[:, i] = least_squares(Ws, vs0[:,i])
             self.us = self.solver[0,0].get_homo()
+
+    return "Hello", "None"
 
 
 
