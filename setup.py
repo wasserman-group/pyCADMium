6b38d42d582f8f575cef00b14cc10e1104deb505
Victor H. Chavez <gonza445@purdue.edu>
2020-10-04 13:35:40 -0400

    Add setuptools and wheel as requirements in setup

--- setup.py
+++ setup.py
@@ -46,7 +46,10 @@ setup(
 
     # Additional entries you may want simply uncomment the lines you want and fill in the data
     # url='http://www.my_package.com',  # Website
-    install_requires=["numpy", "scipy"],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    install_requires=["numpy", 
+                      "scipy",
+                      "setuptools",
+                      "wheel"],              # Required packages, pulls from pip if needed; do not use for Conda deployment
     # platforms=['Linux',
     #            'Mac OS-X',
     #            'Unix',
