7841ae9ae93e7bad130b52d9f4976cc5a0978ff6
Victor H. Chavez <gonza445@purdue.edu>
2020-10-20 16:06:14 -0400

    update orbital invert

--- CADMium/inverter/orbitalinvert.py
+++ CADMium/inverter/orbitalinvert.py
@@ -10,6 +10,55 @@ from scipy.sparse import spdiags
 
 import sys
 
+def ab_symmetrize(x, Nelem, Nmo, North, self):
+    """
+    """
+
+    phi   = np.reshape( x[0:Nelem * np.sum(Nmo)], (Nelem, np.sum(Nmo)), order="F")
+    evals = np.vstack( (x[0 : (np.sum(Nmo) - 2) + np.sum(Nmo) * Nelem], np.array([[0]])) )
+
+    Nmo = Nmo[:, None]
+
+    #Check for degenerate and nearly degenerate orbitals
+    for i in range(0, int(np.sum(Nmo, axis=1)-2)):
+        if np.abs( evals[i] - evals[i+1] ) < 1e-6:
+
+            even =  phi[:, i]   + self.grid.mirror( phi[:,i]   ) \
+                  + phi[:, i+1] + self.grid.mirror( phi[:,i+1] )
+            odd  =  phi[:, i]   - self.grid.mirror( phi[:,i]   ) \
+                  + phi[:, i+1] - self.grid.mirror( phi[:,i+1] )
+
+            phi[:, i]   = even / np.linalg.norm(even)
+            phi[:, i+1] = odd  / np.linalg.norm(odd)
+
+    for i in range(0, int(np.sum(Nmo, axis=1) - 1)):
+        if phi[:, i].T @ self.grid.mirror( phi[:, i] ) > 0:
+            phi[:, i] = phi[:, i] + self.grid.mirror( phi[:,i] )
+            phi[:, i] = phi[:, i] / np.linalg.norm( phi[:, i] )
+
+        else:
+            phi[:, i] = phi[:, i] - self.grid.mirror( phi[:, i] )
+            phi[:, i] = phi[:, i] / np.linalg.norm( phi[:, i] )
+
+    x[0:Nelem * np.sum(Nmo)] = phi.flatten("F")[:, None]
+    v = x[ np.array(range(0, Nelem)) + np.sum(Nmo) * Nelem + np.sum(Nmo) - 1 + North ]
+    v = 0.5 * ( v + self.grid.mirror(v))
+    x[ np.array(range(0, Nelem)) + np.sum(Nmo) * np.sum(Nmo) -1 + North] = v
+    x = x.real
+
+    return x
+
+def normalize(x, Nmo, Nelem, WWi, occ):
+    """
+    """
+
+    phi = np.reshape( x[0:Nelem*np.sum(Nmo)], (Nelem, np.sum(Nmo)) )
+    S = np.sum( WWi @ np.abs( phi )**2 )
+    phi = phi @ np.diag( (occ/S)**0.5 )
+    x[0:Nelem * np.sum(Nmo)] = phi.flatten("F")[:, None]
+
+    return x
+
 def orbitalinvert(self, n0, vs0, phi0, e0, ispin):
     """
     Orbital Invert
@@ -38,23 +87,25 @@ def orbitalinvert(self, n0, vs0, phi0, e0, ispin):
     WWi = W @ Wi
 
     isolver = self.solver[:, ispin]
-    Nmo, N, m, = [], [], []
-    for i in isolver:
-        i.hamiltonian()
-        Nmo.append(i.Nmo)
-        N.append(i.N)
-        m.append(i.m)
+    Nsol = isolver.shape[0]
+    #Nmo, N, m, = [], [], []
+    Nmo, N, m = np.empty((Nsol), dtype=int), np.empty((Nsol), dtype=int), np.empty((Nsol), dtype=int)
+    for i, j in enumerate(isolver):
+        j.hamiltonian()
+        Nmo[i] = j.Nmo
+        N[i] = j.N
+        m[i] = j.m
     Nsol = isolver.shape[0]
 
     #Transforming cell array from matlab to numpy array
-    C = np.empty((Nsol,1), dtype=object)
-    D = np.empty((Nsol,1), dtype=object)
+    C = np.empty((Nsol), dtype=object)
+    D = np.empty((Nsol), dtype=object)
 
     for it in range(Nsol):
-        C[it,0] = matlab.repmat( Wi @ isolver[it].H0, Nmo[it], 1 )
+        C[it] = matlab.repmat( Wi @ isolver[it].H0, Nmo[it], 1 )
 
         if m[it] == 0:            
-            D[it,0] = 2 * np.ones( (Nmo[it], 1) )
+            D[it] = 2 * np.ones( (Nmo[it], 1) )
 
             if isolver[it].pol == 1:
                 nu = N[it] / 2 - np.floor(N[it] / 2)
@@ -62,28 +113,69 @@ def orbitalinvert(self, n0, vs0, phi0, e0, ispin):
                 nu = N[it] - np.floor(N[it])
             
             if nu != 0:
-                D[it,0][Nmo[it]] = D[it,0][Nmo[it]] * nu
+                D[it][Nmo[it]] = D[it][Nmo[it]] * nu
         
         else:
-            D[it,0] = 4 *  np.ones((Nmo[it,0], 1))
+            D[it] = 4 *  np.ones((Nmo[it,0], 1))
             if isolver[it].pol == 1:
                 nu = N[it] / 4 - np.floor(N[it] / 4)
             else:
                 nu = N[it] / 2 - np.floor(N[it] / 2)
 
             if nu != 0:
-                D[it,0][Nmo[it]] = D[it,0][Nmo[it]] * nu
+                D[it][Nmo[it]] = D[it][Nmo[it]] * nu
 
-    print("This is C", C[0,0])
-
-    print(C[:,0])
-
-    C = np.vstack( (C[:]))
+    C = np.squeeze(np.vstack((C[:])))
     H = blkdiag(  C[:] )
-    occ = np.vstack(D[:] )
-            
+    occ = np.squeeze(np.vstack(D[:]))
+
+    B2i = np.array( range( 0, np.sum(Nmo) * Nelem ) )[:, None]
+    B2j = matlab.repmat( np.array(range(0,Nelem))[:,None], np.sum(Nmo), 1 )
+    B3i = np.array( range( 0,(np.sum(Nmo) - 1) * Nelem ) )[:, None]
+    B3j = np.zeros( ((np.sum(Nmo) - 1) * Nelem, 1) )
+
+    for it in range( 1, int(np.sum(Nmo) + 0) ):
+        B3j[ np.linspace(0,Nelem-1, Nelem, dtype=int) + (it - 1) * Nelem ] = it
+
+
+    #Settings for forcing orthogonality between orbitals
+    i_orth = []
+    North = len( i_orth )
+
+    #Initial Guess
+    if isolver[0].phi is None:
+        X = np.vstack((  phi0[np.array(range(0,np.sum(Nmo) * Nelem) ,dtype=int)][:, None], 
+                         (e0[0:np.sum(Nmo)-1] - e0[np.sum(Nmo)])[:, None], 
+                         np.zeros((North, 1)),
+                         vs0 - e0[np.sum(Nmo)]
+                        ))
+
+    else:
+        print("WARNING, Concatenation will probably be wrong")
+        phi   = isolver[0].phi[:,0]
+        evals = isolver[0].eig[0]
+        for i in range(1,Nsol):
+            phi   = np.hstack( (phi, isolver[i].phi[:,i]) )
+            evals = np.hstack( (evals, isolver[i].eig[i]) )
+        X = np.vstack(( phi, 
+                        evals[0:-2] - evals[-1],
+                        np.zeros((North, 1)),
+                        isolve[0].veff - evals[-1]
+                      ))
+
+    if self.optInversion["AB_SYM"] is True:
+        X = ab_symmetrize(X, Nelem, Nmo, North, self)
+
+    X = normalize(X, Nmo, Nelem, WWi, occ)
+
+    print(X)
+        
 
     sys.exit()
+
+
+
+
 
 
 
