cad1d40b0ae9262ba72565693f100f0c1e31fa20
V.H. Chavez <gonza445@purdue.edu>
2021-02-13 18:29:21 -0500

    Replace options validations with pydantic

--- CADMium/kohnsham/kohnsham.py
+++ CADMium/kohnsham/kohnsham.py
@@ -2,8 +2,10 @@
 kohnsham.py
 """
 
+import argparse
 from dataclasses import dataclass
 from multiprocessing import Process, Manager, current_process
+from pydantic import validator, BaseModel
 
 import numpy as np
 
@@ -25,20 +27,40 @@ class E:
     Eks: np.ndarray
     Vks: np.ndarray
 
+class KohnShamOptions(BaseModel):
+    interaction_type : str = 'dft'
+    sym : bool = False
+    fractional : bool = False
+    xfunc_id : int = 1
+    cfunc_id : int = 12
+    xc_family : str = 'lda'
+
+    @validator('interaction_type')
+    def interaction_type_values(cls, v):
+        values = ['dft', 'ni']
+        if v not in values:
+            raise ValueError(f"'interaction_type' must be one of the options: {values}")
+        return v
+
+    @validator('xc_family')
+    def xc_family_values(cls, v):
+        values = ['lda', 'gga']
+        if v not in values:
+            raise ValueError(f"'xc_family' must be one of the options: {values}")
+
 class Kohnsham():
     """
-    Handles a standard kohn sham calculation
+    Handles a standard Kohn-Sham calculation
     """
 
     def __init__(self, grid, Za, Zb, pol, Nmo, N, optKS={}):
 
-        optKS["interaction_type"] = optKS["interaction_type"] if "interaction_type" in optKS.keys() else "dft"
-        optKS["SYM"]              = optKS["SYM"] if "SYM" in optKS.keys() else False
-        optKS["FRACTIONAL"]       = optKS["FRACTIONAL"] if "FRACTIONAL" in optKS.keys() else False
-        optKS["x_func_id"]        = optKS["x_func_id"] if "x_func_id" in optKS.keys() else 1
-        optKS["c_func_id"]        = optKS["c_func_id"] if "c_func_id" in optKS.keys() else 12
-        optKS["xc_family"]        = optKS["xc_family"] if "xc_family" in optKS.keys() else "lda"
-
+        #Validate options
+        for i in optKS.keys():
+            i = i.lower()
+            if i not in KohnShamOptions().dict().keys():
+                raise ValueError(f"{i} is not a valid option for KohnSham")
+        optKS = KohnShamOptions(**optKS)
 
         #Options
         self.optKS = optKS
@@ -60,7 +82,7 @@ class Kohnsham():
 
         #Handle for the solver object
         self.solver = Pssolver(self.grid, self.Nmo, self.N,
-                               optKS["FRACTIONAL"], optKS["SYM"])
+                               optKS.fractional, optKS.sym)
 
         #Nuclear charges
         self.Za = Za
@@ -89,12 +111,12 @@ class Kohnsham():
         self.calc_nuclear_potential()
 
         #Libxc/Hartree handles for fragment calculations
-        if self.optKS["interaction_type"] == 'dft':
-            self.exchange = Libxc(self.grid, self.optKS["xc_family"], self.optKS["x_func_id"])
-            self.correlation = Libxc(self.grid, self.optKS["xc_family"], self.optKS["c_func_id"])
+        if optKS.interaction_type == 'dft':
+            self.exchange = Libxc(self.grid, optKS.xc_family, optKS.xfunc_id)
+            self.correlation = Libxc(self.grid, optKS.xc_family, self.optKS.cfunc_id)
             self.hartree = Hartree(grid)
-        elif self.optKS["interaction_type"] == 'hfs':
-            self.exchange = Libxc(self.grid, self.optKS["xc_family"], self.optKS["x_func_id"])
+        elif optKS.interaction_type == 'hfs':
+            self.exchange = Libxc(self.grid, optKS.xc_family, optKS.xfunc_id)
             self.correlation = 0.0
             self.hartree = Hartree(grid)
         else:
@@ -107,7 +129,7 @@ class Kohnsham():
             for j in range(self.Nmo.shape[1]):
                 self.solver[i,j].hamiltonian()
 
-                if self.optKS["interaction_type"] == "ni":
+                if self.optKS.interaction_type == "ni":
                     self.solver[i,j].e0 = -1.5 * max(self.Za, self.Zb)**2 / (self.solver[i,j].m + 1)**2
                 else:
                     self.solver[i,j].e0 = - max(self.Za, self.Zb)**2 / (self.solver[i,j].m + 1)**2 
@@ -132,7 +154,7 @@ class Kohnsham():
             self.vnuc[:, 0] = v
             self.vnuc[:, 1] = v
 
-        if self.optKS["SYM"] is True:
+        if self.optKS.sym is True:
             self.vnuc = 0.5 * (self.vnuc + self.grid.mirror(self.vnuc))
         
     def set_effective_potential(self):
@@ -218,13 +240,13 @@ class Kohnsham():
 
                 np.append(self.E.evals, self.solver[i,j])
 
-        if self.optKS["interaction_type"] == 'ni':
+        if self.optKS.interaction_type == 'ni':
             self.E.Vnuc = self.grid.integrate(np.sum(self.n * self.vnuc, axis=1))
             self.E.Vext = self.grid.integrate(np.sum(self.n * self.vext, axis=1))
             self.E.Enuc = self.grid.integrate(np.sum(self.n * self.vnuc, axis=1))
             self.E.Et = self.E.Ts + self.E.Enuc
 
-        elif self.optKS["interaction_type"] == 'dft':
+        elif self.optKS.interaction_type == 'dft':
 
             self.E.Enuc = self.grid.integrate(np.sum(self.n * self.vnuc, axis=1))
             self.E.Vext = self.grid.integrate(np.sum(self.n * self.vext, axis=1))
@@ -239,7 +261,7 @@ class Kohnsham():
 
             self.E.Et = self.E.Ts + self.E.Enuc + self.E.Eh + self.E.Ex + self.E.Ec
 
-        elif self.optKS["interaction_type"] == "hfs":
+        elif self.optKS.interaction_type == "hfs":
 
             self.E.Enuc = self.grid.integrate(np.sum(self.n * self.vnuc, axis=1))
             self.E.Vext = self.grid.integrate(np.sum(self.n * self.vext, axis=1))
@@ -275,10 +297,10 @@ class Kohnsham():
 
         #Fragment effective potentials
 
-        if self.optKS["interaction_type"] == "ni":
+        if self.optKS.interaction_type == "ni":
             self.vhxc = np.zeros_like(self.vext)
 
-        elif self.optKS["interaction_type"] == "dft":
+        elif self.optKS.interaction_type == "dft":
             #Get effective potential for each element in ensemble
             
             self.E.Ex, self.V.vx = self.exchange.get_xc(self.n)
@@ -287,7 +309,7 @@ class Kohnsham():
 
             self.vhxc = self.V.vx + self.V.vc + self.V.vh
 
-        if self.optKS["SYM"] is True:
+        if self.optKS.sym is True:
             self.vhxc = 0.5 * (self.vhxc + self.grid.mirror(self.vhxc))
 
     def calc_chempot(self):
