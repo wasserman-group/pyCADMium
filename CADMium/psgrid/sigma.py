5aa589948f1cda49a7312e301dc94f9d83ef0019
V.H. Chavez <gonza445@purdue.edu>
2021-02-18 15:40:39 -0500

    Fix typos in sigma for GGA calculations

--- CADMium/psgrid/sigma.py
+++ CADMium/psgrid/sigma.py
@@ -2,6 +2,8 @@
 sigma.py
 """
 
+import numpy as np 
+
 def sigma(self, n):
 
     pol = n.shape[1] 
@@ -9,11 +11,13 @@ def sigma(self, n):
     if pol == 1:
         sigma = (self.grada @ n) ** 2 + (self.gradr @ n) ** 2
 
-    elif pol == 3:
-        sigma = np.zeros((n, 3))
+    elif pol == 2:
+        npoints = n.shape[0]
+        sigma = np.zeros((npoints, 3))
         sigma[:,2] = (self.grada @ n[:, 1]) ** 2 +  (self.gradr @ n[:, 1]) ** 2
-        sigma[:,0] = (self.grada @ n[:, 2]) ** 2 +  (self.gradr @ n[:, 2]) ** 2
-        sigma[:,1] = self.grada @ n[:, 0] * self.grada @ n[:,1] + self.gradr @ n[:, 0] * self.gradr @ n[:,1]
+        sigma[:,0] = (self.grada @ n[:, 0]) ** 2 +  (self.gradr @ n[:, 0]) ** 2
+        sigma[:,1] = self.grada @ n[:, 0] * self.grada @ n[:,1] + \ 
+                     self.gradr @ n[:, 0] * self.gradr @ n[:,1]
 
     else:
         raise ValueError("Shape of density in second axis should not be greater than 2")
